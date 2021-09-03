e90171bb783f4c4b3dd314873125280ed9c3c754
calliope <calliope183@gmail.com>
2021-09-03 16:35:03 +0900

    perm

--- myauth/authentications.py
+++ myauth/authentications.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken
 
 
 class CustomJWTAuthentication(JWTAuthentication):
@@ -21,7 +22,10 @@ class CustomJWTAuthentication(JWTAuthentication):
         if raw_token is None:
             return None
 
-        validated_token = self.get_validated_token(raw_token)
+        try:
+            validated_token = self.get_validated_token(raw_token)
+        except InvalidToken:
+            return None
 
         return self.get_user(validated_token), validated_token
 
